400994a03ce30076da6b4f419f3c5381b89d637e
mougrim <rinat@mougrim.ru>
2011-09-17 10:40:56 +0400

    add project files

---
+++ CCSV.php
@@ -0,0 +1,222 @@
+<?php
+/**
+ * CCSV class file.
+ *
+ * @author Mougrim <rinat@mougrim.ru>
+ */
+
+/**
+ * CCSV базовый класс для работы с CSV
+ *
+ * @author Mougrim <rinat@mougrim.ru>
+ * @package lib.csv
+ */
+abstract class CCSV
+{
+	/**
+	 * @var string $sTerminated разделитель строк
+	 */
+	private $_terminated = "\r\n";
+	/**
+	 * @var string $sSeparator разделитель ячеек
+	 */
+	private $_separator = "	";
+	/**
+	 * @var string $sEnclosed символ обрамления строк с запрещенными символами
+	 */
+	private $_enclosed = '"';
+	/**
+	 * @var string $sEscaped символ, который экренирует $this->enclosed
+	 */
+	private $_escaped = '"';
+	/**
+	 * @var string $sCharser конечная кодировка
+	 */
+	private $_charset = 'UCS-2LE';
+	/**
+	 * 00 00 FE FF UTF-32, big-endian
+	 * FF FE 00 00 UTF-32, little-endian
+	 * FE FF UTF-16, big-endian
+	 * FF FE UTF-16, little-endian
+	 * EF BB BF UTF-8
+	 *
+	 * @var array $hBOMHeaders хеш ([кодировка] => [BOM header])
+	 */
+	private $_BOMHeaders = array(
+		'UCS-2LE'  => "\xFF\xFE",
+		'UTF-8'    => "\xEF\xBB\xBF",
+		'UTF-16LE' => "\xFF\xFE",
+		'UTF-16BE' => "\xFE\xFF",
+		'UTF-32LE' => "\xFF\xFE",
+		'UTF-32BE' => "\x00\x00\xFE\xFF",
+	);
+
+	/**
+	 * Конвертирование строки в кодировку $this->charset
+	 *
+	 * @param string $csv
+	 * @return string
+	 */
+	public function convert($csv)
+	{
+		if($this->getCharset() == 'UTF-8')
+		{
+			return $csv;
+		}
+
+		$csv = iconv('UTF-8', $this->getCharset(), $csv);
+
+		return $csv;
+	}
+
+	/**
+	 * Конвертирование строки в кодировку UTF-8 из $this->charset
+	 *
+	 * @param string $csv
+	 * @return string
+	 */
+	public function unConvert($csv)
+	{
+		if($this->getCharset() == 'UTF-8')
+		{
+			return $csv;
+		}
+
+		$csv = iconv($this->getCharset(), 'UTF-8', $csv);
+
+		return $csv;
+	}
+
+	/**
+	 * Добавление BOM-заголовков к строке
+	 *
+	 * @param string $csv
+	 * @return string
+	 */
+	public function addBOMHeaders($csv)
+	{
+		if($this->getBOMHeader())
+		{
+			$csv = $this->getBOMHeader() . $csv;
+		}
+
+		return $csv;
+	}
+
+	/**
+	 * Функция удаляет BOM-заголовки
+	 *
+	 * @param string $csv
+	 * @return string
+	 */
+	public function removeBOMHeaders($csv)
+	{
+		$BOMHeader = $this->getBOMHeader();
+
+		if($BOMHeader === null || strlen($BOMHeader) > strlen($csv))
+		{
+			return $csv;
+		}
+
+		if($BOMHeader === substr($csv, 0, strlen($BOMHeader)))
+		{
+			$csv = substr($csv, strlen($BOMHeader));
+		}
+
+		return $csv;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getBOMHeader()
+	{
+		if(array_key_exists($this->getCharset(), $this->_BOMHeaders))
+		{
+			return $this->_BOMHeaders[$this->getCharset()];
+		}
+
+		return null;
+	}
+
+	/**
+	 * @param string $charset
+	 */
+	public function setCharset($charset)
+	{
+		$this->_charset = $charset;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getCharset()
+	{
+		return $this->_charset;
+	}
+
+	/**
+	 * @param string $enclosed
+	 */
+	public function setEnclosed($enclosed)
+	{
+		$this->_enclosed = $enclosed;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getEnclosed()
+	{
+		return $this->_enclosed;
+	}
+
+	/**
+	 * @param string $escaped
+	 */
+	public function setEscaped($escaped)
+	{
+		$this->_escaped = $escaped;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getEscaped()
+	{
+		return $this->_escaped;
+	}
+
+	/**
+	 * @param string $separator
+	 */
+	public function setSeparator($separator)
+	{
+		$this->_separator = $separator;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getSeparator()
+	{
+		return $this->_separator;
+	}
+
+	/**
+	 * @param string $terminated
+	 */
+	public function setTerminated($terminated)
+	{
+		$this->_terminated = $terminated;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getTerminated()
+	{
+		return $this->_terminated;
+	}
+
+}
---
+++ CCSVExport.php
@@ -0,0 +1,172 @@
+<?php
+/**
+ * CCSVExport class file.
+ *
+ * @author Mougrim <rinat@mougrim.ru>
+ */
+
+/**
+ * CCSVExport класс позволяющий делать экспорт в формат CSV
+ *
+ * @author Mougrim <rinat@mougrim.ru>
+ * @package lib.csv
+ */
+class CCSVExport extends CCSV
+{
+	/**
+	 * @var string $sContentType заголовок Content-type, если значение пустое, то заголовок не выдается
+	 */
+	private $_contentType = 'text/x-csv';
+	protected $csvRows = array();
+
+	/**
+	 * @param array $headers
+	 * @param array $content
+	 */
+	public function __construct(array $headers = array(), array $content = array())
+	{
+		$this->addTableWithHeaders($headers, $content);
+	}
+
+	public function addRow($row)
+	{
+		$this->csvRows[] = $row;
+	}
+
+	public function addRowByKeys($keys, $data)
+	{
+		$row = array();
+
+		foreach($keys as $key)
+		{
+			$row[] = $data[$key];
+		}
+
+		$this->addRow($row);
+	}
+
+	public function addTable($table)
+	{
+		$this->csvRows = array_merge($this->csvRows, $table);
+	}
+
+	public function addTableByKeys($keys, $table)
+	{
+		foreach($table AS $data)
+		{
+			$this->addRowByKeys($keys, $data);
+		}
+	}
+
+	public function addTableWithHeaders($headers, $table)
+	{
+		$this->addRow($headers);
+		$this->addTableByKeys(array_keys($headers), $table);
+	}
+
+	/**
+	 * @param string $string строка, которую нужно обрамить символом $this->enclosed
+	 * @return string обрамленная строка
+	 */
+	protected function prepareStr($string)
+	{
+		return $this->getEnclosed() . str_replace($this->getEnclosed(), $this->getEscaped() . $this->getEnclosed(), $string) . $this->getEnclosed();
+	}
+
+	/**
+	 * @return string данные в формате CSV
+	 */
+	public function toCSV()
+	{
+
+		$csvRows = array();
+
+		foreach($this->csvRows as $row)
+		{
+			$csvRow = array();
+
+			foreach($row as $value)
+			{
+				$csvRow[] = $this->createField($value);
+			}
+
+			$csvRows[] = implode($this->getSeparator(), $csvRow);
+		}
+
+		$csv = implode($this->getTerminated(), $csvRows);
+
+		$csv = $this->addBOMHeaders($this->convert($csv));
+
+		return $csv;
+	}
+
+	/**
+	 * Создание ячейки. Если соддержимое не число, то оно обрамляется функцией $this->prepareStr().
+	 *
+	 * @param mixed $field содержимое ячейки
+	 * @return string созданная ячейка в виде строки
+	 */
+	protected function createField($field)
+	{
+		if(!is_float($field) && !is_int($field))
+		{
+			$field = $this->prepareStr($field);
+		}
+
+		return $field;
+	}
+
+	/**
+	 * Отправление файла CSV в браузер
+	 * @param string $fileName название файла
+	 */
+	public function sendFile($fileName = '')
+	{
+		$output = $this->toCSV();
+
+		$fileName = $fileName . '_' . date("Y-m-d_H-i", time()) . '.csv';
+		header("Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0");
+
+		// Output to browser with appropriate mime type, you choose ;)
+		if($this->getContentType() != '')
+		{
+			header("Content-type: {$this->getContentType()}");
+		}
+
+		header("Content-Disposition: attachment; filename={$fileName}");
+		echo $output;
+	}
+
+	/**
+	 * Сохранение CSV в файл
+	 *
+	 * @param string $pathToFile путь, куда нужно сохранить файл
+	 * @param boolean $isOverwrite
+	 * @return boolean
+	 */
+	public function saveAs($pathToFile, $isOverwrite = true)
+	{
+		if(!$isOverwrite && file_exists($pathToFile))
+		{
+			return false;
+		}
+
+		return (boolean) file_put_contents($pathToFile, $this->toCSV());
+	}
+
+	/**
+	 * @param string $contentType заголовок Content-type, если значение пустое, то заголовок не выдается
+	 */
+	public function setContentType($contentType)
+	{
+		$this->_contentType = $contentType;
+	}
+
+	/**
+	 * @return string заголовок Content-type
+	 */
+	public function getContentType()
+	{
+		return $this->_contentType;
+	}
+}
